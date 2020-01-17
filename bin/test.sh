3c93819058959127974747060128bf331d3e8b95
Seandon Mooy <seandon.mooy@gmail.com>
2020-01-16 16:20:10 -0800

    add test wrapper, gateway work

---
+++ bin/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TEST_TARGET="${1:-test-gateway}"
+
+kubectl exec -it $(kubectl get pods -l app=kubesail-gateway -o name | awk -F'/' '{print $2}') bin/${TEST_TARGET}
--- test/gateway/index.js
+++ test/gateway/index.js
@@ -1,15 +1,60 @@
 // @flow
 
 // $FlowIssue
+const http = require('http')
+const https = require('https')
 const { expect } = require('chai')
+
+const { GATEWAY_HTTP_LISTEN_PORT, GATEWAY_HTTPS_LISTEN_PORT } = require('../../lib/shared/config')
+
+const httpReqOpts = {
+  hostname: '127.0.0.1',
+  port: GATEWAY_HTTP_LISTEN_PORT,
+  method: 'GET'
+}
+const httpsReqOpts = {
+  hostname: '127.0.0.1',
+  port: GATEWAY_HTTPS_LISTEN_PORT,
+  method: 'GET'
+}
 
 const describe = global.describe
 const it = global.it
 
 describe('Gateway tests', function() {
   describe('HTTP Tunnel handling', function() {
-    it('does nothing yet', async function() {
-      expect(1).to.equal(1)
+    it('Returns a 503 when a bad host is requested', function(done) {
+      const req = http.request(
+        Object.assign({}, httpReqOpts, {
+          headers: { host: 'foobar.com' }
+        }),
+        res => {
+          expect(res.statusCode).to.equal(503)
+          done()
+        }
+      )
+      req.on('error', error => {
+        console.error(error)
+      })
+      req.end()
+    })
+  })
+
+  describe('HTTPS Tunnel handling', function() {
+    it('Returns a 503 when a bad host is requested', function(done) {
+      const req = https.request(
+        Object.assign({}, httpsReqOpts, {
+          headers: { host: 'foobar.com' }
+        }),
+        res => {
+          expect(res.statusCode).to.equal(503)
+          done()
+        }
+      )
+      req.on('error', error => {
+        console.error(error)
+      })
+      req.end()
     })
   })
 })
