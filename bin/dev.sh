05775607b075c64d1d80d0b6cff1d16435d4d77e
Seandon Mooy <seandon.mooy@gmail.com>
2020-02-04 23:13:13 -0800

    use agent username

--- bin/dev.sh
+++ bin/dev.sh
@@ -6,10 +6,10 @@ set -euf -o pipefail
 LOCAL_DEV_SECRET_FILE="k8s/overlays/dev/secrets/env.plain"
 
 if [ ! -f ${LOCAL_DEV_SECRET_FILE} ]; then
-  read -p "Please enter your KubeSail username:" -n 1 -r
+  read -p "Please enter your KubeSail username:" -r
   echo
 
-  echo "$REPLY" > $LOCAL_DEV_SECRET_FILE
+  echo "KUBESAIL_AGENT_USERNAME=$REPLY" > $LOCAL_DEV_SECRET_FILE
 fi
 
 skaffold dev --port-forward
