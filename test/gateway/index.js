c267fc8c1710cbfcca13155b5a2304913786ae49
Seandon Mooy <seandon.mooy@gmail.com>
2020-02-29 18:27:16 -0800

    fix for internal HTTPS responder, end-to-end qotm working properly again!

--- test/gateway/index.js
+++ test/gateway/index.js
@@ -14,7 +14,9 @@ const httpReqOpts = {
 const httpsReqOpts = {
   hostname: '127.0.0.1',
   port: GATEWAY_HTTPS_LISTEN_PORT,
-  method: 'GET'
+  method: 'GET',
+  insecure: true,
+  rejectUnauthorized: false
 }
 
 const describe = global.describe
@@ -77,33 +79,14 @@ describe('Gateway tests', function() {
       const req = https.request(
         Object.assign({}, httpsReqOpts, {
           headers: { host: 'foobar.com' }
-        })
-      )
-      req.on('error', error => {
-        expect(error.code).to.equal('ECONNRESET')
-        done()
-      })
-      req.end()
-    })
-
-    it('Returns a 200 when a good host is requested (qotm end-to-end)', function(done) {
-      const req = http.request(
-        Object.assign({}, httpsReqOpts, {
-          headers: { host: 'test-qotm.example.com' }
         }),
         res => {
-          res.once('data', data => {
-            const json = JSON.parse(data.toString())
-            expect(json.ok).to.equal(true)
-          })
-          res.on('end', () => {
-            expect(res.statusCode).to.equal(200)
-            done()
-          })
+          expect(res.statusCode).to.equal(503)
+          done()
         }
       )
       req.on('error', error => {
-        console.error(error)
+        console.error('Drops the connection test error', error)
       })
       req.end()
     })
@@ -111,20 +94,18 @@ describe('Gateway tests', function() {
     it('Returns a 200 when a good host is requested (qotm end-to-end)', function(done) {
       const req = https.request(
         Object.assign({}, httpsReqOpts, {
-          headers: { host: 'test-qotm.example.com' },
-          insecure: true,
-          rejectUnauthorized: false
+          headers: { host: 'test-qotm.example.com' }
         }),
         res => {
-          res.once('data', data => {
-            const json = JSON.parse(data.toString())
-            expect(res.statusCode).to.equal(200)
-            expect(json.ok).to.equal(true)
+          expect(res.statusCode).to.equal(200)
+          res.on('data', data => {
+            console.log(data.toString())
           })
+          done()
         }
       )
       req.on('error', error => {
-        console.error(error)
+        console.error('Returns a 200 when a good host', error)
       })
       req.end()
     })
