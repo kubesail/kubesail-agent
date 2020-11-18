5a505c570d590e1b7965b30e9e6fcf0a911bf9a7
Seandon Mooy <seandon.mooy@gmail.com>
2020-11-18 14:19:15 -0800

    add some rough instructions for buildx in the bin/build script

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,23 @@
 
 TAG="kubesail/agent:v$(cat VERSION.txt)"
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t ${TAG}  --push .
+# Enable docker experimental mode:
+#  - echo '{ "experimental": true }' > /etc/docker/daemon.json
+#  - echo '{ "experimental": "enabled" }' > ~/.docker/config.json
+# install buildx (https://github.com/docker/buildx/releases)
+
+# docker buildx create --name mybuilder
+# docker buildx use mybuilder
+# docker buildx inspect --bootstrap
+# docker run --name binfmt --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
+
+./bin/generate_self_signed_cert.sh
+
+BUILDX_COMMAND="docker buildx"
+command -v buildx > /dev/null && BUILDX_COMMAND="buildx"
+
+${BUILDX_COMMAND} build \
+  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  -t ${TAG} \
+  --push \
+  .
