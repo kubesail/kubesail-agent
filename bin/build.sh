0d57d2f765095b5c965356a8ded7a632372e1989
Seandon Mooy <seandon.mooy@gmail.com>
2020-11-30 15:16:53 -0800

    debug a bit better when failing to get the ingresses api

--- bin/build.sh
+++ bin/build.sh
@@ -12,6 +12,9 @@ TAG="kubesail/agent:v$(cat VERSION.txt)"
 # docker buildx inspect --bootstrap
 # docker run --name binfmt --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
 
+# Troubleshooting:
+# buildx stop... buildx inspect --bootstrap
+
 ./bin/generate_self_signed_cert.sh
 
 BUILDX_COMMAND="docker buildx"
