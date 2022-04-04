47f64074aa8e06a31cb4fef3e5961da29de4e751
Seandon Mooy <seandon.mooy@gmail.com>
2022-04-04 21:35:58 +0000

    0.45.8

--- bin/node.sh
+++ bin/node.sh
@@ -8,7 +8,7 @@ fi
 
 if [[ "$1" == "agent" ]]; then
   mkdir -p /var/run/pibox
-  FB_VERSION=v1
+  FB_VERSION="v2"
   FB_PATH=/var/run/pibox/pibox-framebuffer-$FB_VERSION
   if [[ ! -f $FB_PATH ]]; then
     curl --connect-timeout 10 -sLo $FB_PATH https://github.com/kubesail/pibox-framebuffer/releases/download/$FB_VERSION/pibox-framebuffer
