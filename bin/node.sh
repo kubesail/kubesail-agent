7ff2a33286391da3bb3ecb88c43c5af878836528
Seandon Mooy <seandon.mooy@gmail.com>
2022-04-19 14:31:44 -0700

    fix framebuffer path

--- bin/node.sh
+++ bin/node.sh
@@ -7,14 +7,14 @@ if [[ "$1" == "gateway" ]]; then
 fi
 
 if [[ "$1" == "agent" ]]; then
-  mkdir -p /var/run/pibox &> /dev/null
+  mkdir -p /opt/kubesail &> /dev/null
   FB_VERSION="v6"
-  FB_PATH=/var/run/pibox/pibox-framebuffer-$FB_VERSION
-  if [[ ! -f $FB_PATH && -d /var/run/pibox ]]; then
+  FB_PATH=/opt/kubesail/pibox-framebuffer-$FB_VERSION
+  if [[ ! -f $FB_PATH && -d /opt/kubesail ]]; then
     curl --connect-timeout 10 -sLo $FB_PATH https://github.com/kubesail/pibox-framebuffer/releases/download/$FB_VERSION/pibox-framebuffer
     chmod +x $FB_PATH
-    rm -f /var/run/pibox/pibox-framebuffer
-    ln -s $FB_PATH /var/run/pibox/pibox-framebuffer
+    rm -f /opt/kubesail/pibox-framebuffer
+    ln -s $FB_PATH /opt/kubesail/pibox-framebuffer
   fi
 fi
 
