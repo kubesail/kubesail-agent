b0cfb493d39c25d59d052feb49174bfa0c597f24
Seandon Mooy <seandon.mooy@gmail.com>
2023-12-28 15:55:18 -0800

    fully rewrite DNS fallback

--- test/agent/index.js
+++ test/agent/index.js
@@ -1,8 +1,8 @@
 // @flow
 
-const http = require('http')
+const http = require('node:http')
 // $FlowIssue
-const { expect } = require('chai')
+// const { expect } = require('chai')
 
 const { KUBESAIL_AGENT_HTTP_LISTEN_PORT } = require('../../lib/shared/config')
 
@@ -11,9 +11,9 @@ const httpReqOpts = { hostname: '127.0.0.1', port: KUBESAIL_AGENT_HTTP_LISTEN_PO
 const describe = global.describe
 const it = global.it
 
-describe('Agent tests', function() {
-  describe('HTTP Tunnel handling', function() {
-    it('Currently routes the test endpoint', async function() {
+describe('Agent tests', function () {
+  describe('HTTP Tunnel handling', function () {
+    it('Currently routes the test endpoint', async function () {
       const req = http.request(httpReqOpts, res => {
         res.on('data', d => {
           process.stdout.write(d)
